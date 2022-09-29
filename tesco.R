2629ce422174eaff11cfc4ac483b4f82e798b300
Andrea Gonzales <AndreaGon@users.noreply.github.com>
2022-09-29 15:52:01 +0800

    Clean up

--- tesco.R
+++ tesco.R
@@ -17,27 +17,7 @@ library(GGally)
 
 library(rstudioapi) 
 
-#Data Integration (Without parallel processing)
-system.time({
-  current_path <- dirname(getSourceEditorContext()$path)
-  files <- list.files(path = paste(current_path, "/AreaLevel", sep=""),pattern = ".csv")
-  file_list <- list()
-  file_list <- lapply(files, function(filename){
-    split_filename <- strsplit(filename[1], "_")[1]
-    matrix_filename <- matrix(unlist(split_filename),ncol=3,byrow=T)
-    month <- matrix_filename[1, 1]
-    area <- matrix_filename[1, 2]
-    
-    ret <- read_csv(paste(current_path,"/AreaLevel/", filename, sep=""))
-    ret$month <- month
-    ret$area <- area
-    return(ret)})
-  df <- rbindlist(file_list)
-  
-  write.csv(df,paste(current_path, "/Warehouse/AreaLevel_month_combined.csv", sep=""), row.names = FALSE)
-})
-
-#With Parallel processing
+#Data Integration With Parallel processing
 system.time({
   current_path <- dirname(getSourceEditorContext()$path)
   files <- list.files(path = paste(current_path, "/AreaLevel", sep=""),pattern = ".csv")
